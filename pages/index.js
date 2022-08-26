722c4de3bbce521969a0feb2957d0367c2dd88dc
SpencerM0415 <76704737+SpencerM0415@users.noreply.github.com>
2022-08-26 12:34:03 -0400

    ipad test

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from 'next/head';
+import Image from 'next/image';
 import { PostCard, Categories, PostWidget } from '../components';
 import { getPosts } from '../services';
 
