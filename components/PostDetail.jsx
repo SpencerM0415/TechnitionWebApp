34476dded4b72b5bfd3b035acc1737100dcda32b
SpencerM <spencer.morse@maine.edu>
2022-09-01 16:37:18 +0000

    Update PostDetail.jsx and PostWidget.jsx

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -3,7 +3,18 @@ import React from "react";
 const PostDetail = ({ post }) => {
   return (
     <div className="bg-white shadow-lg rounded-lg lg:p-8 pb-12 mb-8">
-      PostDetail
+      <div className="relative overflow-hidden shadowmd mb-6"> 
+          <img 
+            src={post.featuredImage.url}
+            alt={post.title}
+            className='object-top h-full w-full rounded-t-lg'
+          />
+      </div>
+      <div className="px-4 lg:px-0">
+          <div className="flex items-center"> 
+              
+          </div>
+      </div>     
     </div>
   );
 };
