079e69f2f71ef2f86fec50b81b179e848e93d149
SpencerM <spencer.morse@maine.edu>
2022-09-01 16:20:04 +0000

    Update 5 files

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
-const PostDetail = () => {
+const PostDetail = ({ post }) => {
   return (
-    <div>PostDetail</div>
-  )
-}
+    <div className="bg-white shadow-lg rounded-lg lg:p-8 pb-12 mb-8">
+      PostDetail
+    </div>
+  );
+};
 
-export default PostDetail
+export default PostDetail;
