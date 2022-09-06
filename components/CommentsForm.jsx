f5feaf993ec88a28a6baac3ab2e797e17388ea99
SpencerM <spencer.morse@maine.edu>
2022-09-06 18:51:28 +0000

    Update 5 files

--- components/CommentsForm.jsx
+++ components/CommentsForm.jsx
@@ -10,7 +10,27 @@ const CommentsForm = ({ slug}) => {
   const storeDataEl = useRef();
   
   const handleCommentSubmission = () => {
+      setError(false);
       
+      const { value: comment } = commentEl.current;
+      const { value: name } = nameEl.current;
+      const { value: email } = emailEl.current;
+      const { checked: storeData } = storeDataEl.current;
+      
+      if(!comment || !name || !email) {
+          setError(true);
+          return;
+      }
+      
+      const commentObj = {name, email, comment, slug}
+      
+      if(storeData) {
+          localStorage.setItem('name', name);
+          localStorage.setItem('email', email);
+      } else {
+          localStorage.removeItem('name', name);
+          localStorage.removeItem('email', email);
+      }
   } 
     
   return (
@@ -41,6 +61,10 @@ const CommentsForm = ({ slug}) => {
             />
         </div>
         <div className='grid grid-cols-1 gap-4 mb-4'>
+            <div>
+                <input ref={storeDataEl} type='checkbox' id='storeData' name='storeData' value='true'/>
+                <label className='text-gray-500 cursor-pointer ml-2' htmlFor='storeData'>Save my name and email for future use.</label>
+            </div>
         </div>
         {error && <p className='text-xs text-red-500'>All fields are required.</p>}
         <div className='mt-8'>
@@ -51,6 +75,7 @@ const CommentsForm = ({ slug}) => {
                 >
             Post Comment    
             </button>
+            {showSuccessMessage && <span className='text-xl float-right font-semibold mt-3 text-green-500'>Comment submitted for review.</span>}
         </div>
     </div>
   )
